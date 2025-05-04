e1f580db5d96ab004f7f8ce3b3a113fdd19870e5
Demi Avaliani <demikoavaliani@gmail.com>
2025-05-04 14:56:14 +0400

    feat: fetch tasks - don't re-fetch if already fetched (till refresh)

---
+++ src/stores/tasks.ts
@@ -0,0 +1,9 @@
+import { defineStore } from 'pinia';
+import { ref } from 'vue';
+import type { Task } from '@/types/TasksType';
+
+export const useTasksStore = defineStore('site', () => {
+	const tasksList = ref<Array<Task> | null>(null);
+
+	return { tasksList };
+});
--- src/views/Tasks.vue
+++ src/views/Tasks.vue
@@ -1,1 +1,24 @@
-<template>Tasks</template>
+<template>
+	<h1>Tasks</h1>
+</template>
+
+<script setup lang="ts">
+	import { onMounted, ref } from 'vue';
+	import { useTasksStore } from '@/stores/tasks';
+	import { storeToRefs } from 'pinia';
+
+	const { tasksList } = storeToRefs(useTasksStore());
+
+	const fetchTasks = async () => {
+		const response = await fetch(
+			'https://q1z3telex7a9metry.denaliops.com/data.json'
+		);
+		return await response.json();
+	};
+
+	onMounted(async () => {
+		if (!tasksList.value) {
+			tasksList.value = await fetchTasks();
+		}
+	});
+</script>
