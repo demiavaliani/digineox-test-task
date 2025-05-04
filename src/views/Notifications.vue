02edd68512e46894754d7416de8a1db8213c9c5e
Demi Avaliani <demikoavaliani@gmail.com>
2025-05-04 14:54:42 +0400

    feat: base h1 styles

--- src/views/Notifications.vue
+++ src/views/Notifications.vue
@@ -1,1 +1,11 @@
-<template>Notifications</template>
+<template>
+	<div class="notifications">
+		<h1>NOTIFICATIONS</h1>
+	</div>
+</template>
+
+<style lang="scss">
+	.notifications {
+		padding: 48px;
+	}
+</style>
--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,1 +1,11 @@
-<template>Settings</template>
+<template>
+	<div class="settings">
+		<h1>SETTINGS</h1>
+	</div>
+</template>
+
+<style lang="scss">
+	.settings {
+		padding: 48px;
+	}
+</style>
